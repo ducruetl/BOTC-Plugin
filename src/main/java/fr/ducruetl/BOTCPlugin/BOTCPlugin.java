e5cad0dccf612a5ac431d74f72ba6fecd2c07ea2
Léo Ducruet <ducruet.leo1@gmail.com>
2026-02-23 14:34:01 +0100

    Added join and quit messages

--- src/main/java/fr/ducruetl/BOTCPlugin/BOTCPlugin.java
+++ src/main/java/fr/ducruetl/BOTCPlugin/BOTCPlugin.java
@@ -1,17 +1,36 @@
 package fr.ducruetl.BOTCPlugin;
 
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.plugin.java.JavaPlugin;
 
-public final class BOTCPlugin extends JavaPlugin {
+public final class BOTCPlugin extends JavaPlugin implements Listener {
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-
+        System.out.println("Plugin BOTC started.");
+        getServer().getPluginManager().registerEvents(this, this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
+        System.out.println("Plugin BOTC stopped.");
     }
+
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        event.setJoinMessage(playerName + " entre dans le village.");
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        String playerName = event.getPlayer().getName();
+        event.setQuitMessage(playerName + " quitte le village. :(");
+    }
+
 }
