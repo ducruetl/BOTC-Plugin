2bda8eeeae5a015741563064cd0baa75d2c078f8
Léo Ducruet <ducruet.leo1@gmail.com>
2026-02-23 08:29:22 +0100

    First Commit

---
+++ src/main/java/fr/ducruetl/BOTCPlugin/BOTCPlugin.java
@@ -0,0 +1,17 @@
+package fr.ducruetl.BOTCPlugin;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class BOTCPlugin extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
