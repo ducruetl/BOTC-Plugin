0cba637947dafc80f24b9c980129eee7c8e07981
Léo Ducruet <ducruet.leo1@gmail.com>
2026-02-24 10:33:27 +0100

    Moved player event handling to a new file, and added a new command to enable/disable nametags

--- src/main/java/fr/ducruetl/BOTCPlugin/BOTCPlugin.java
+++ src/main/java/fr/ducruetl/BOTCPlugin/BOTCPlugin.java
@@ -1,36 +1,68 @@
 package fr.ducruetl.BOTCPlugin;
 
-import org.bukkit.event.EventHandler;
-import org.bukkit.event.Listener;
-import org.bukkit.event.player.PlayerJoinEvent;
-import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
 import org.bukkit.plugin.java.JavaPlugin;
+import org.bukkit.scoreboard.Team;
+import org.bukkit.scoreboard.Team.Option;
+import org.bukkit.scoreboard.Team.OptionStatus;
 
-public final class BOTCPlugin extends JavaPlugin implements Listener {
+import fr.ducruetl.BOTCPlugin.listeners.PlayerListener;
+
+public final class BOTCPlugin extends JavaPlugin {
+
+    public static final String NAMETAG_TEAM_NAME = "hidenametag";
 
     @Override
     public void onEnable() {
         // Plugin startup logic
-        System.out.println("Plugin BOTC started.");
-        getServer().getPluginManager().registerEvents(this, this);
+        getLogger().info("Plugin BOTC started.");
+
+        // Create a new team if it doesn't exist for showing/hiding nametags
+        if (getNametagTeam() == null) {
+            getServer().getScoreboardManager().getMainScoreboard().registerNewTeam(NAMETAG_TEAM_NAME);
+        }
+
+        // Register listeners classes
+        getServer().getPluginManager().registerEvents(new PlayerListener(this), this);
     }
 
     @Override
     public void onDisable() {
         // Plugin shutdown logic
-        System.out.println("Plugin BOTC stopped.");
+        getLogger().info("Plugin BOTC stopped.");
     }
 
-    @EventHandler
-    public void onPlayerJoin(PlayerJoinEvent event) {
-        String playerName = event.getPlayer().getName();
-        event.setJoinMessage(playerName + " entre dans le village.");
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+
+        if (label.contentEquals("nametags")) {
+            if (args.length != 1) {
+                return false;
+            }
+
+            Team nametagTeam = getNametagTeam();
+            if (args[0].contentEquals("enable")) {
+                nametagTeam.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.ALWAYS);
+                sender.sendMessage("Nametags enabled.");
+            } else if (args[0].contentEquals("disable")) {
+                nametagTeam.setOption(Option.NAME_TAG_VISIBILITY, OptionStatus.NEVER);
+                sender.sendMessage("Nametags disabled.");
+            } else {
+                return false;
+            }
+        } else {
+            return false;
+        }
+
+        return true;
     }
 
-    @EventHandler
-    public void onPlayerQuit(PlayerQuitEvent event) {
-        String playerName = event.getPlayer().getName();
-        event.setQuitMessage(playerName + " quitte le village. :(");
+    public Team getNametagTeam() {
+        return getServer()
+                .getScoreboardManager()
+                .getMainScoreboard()
+                .getTeam(NAMETAG_TEAM_NAME);
     }
 
 }
---
+++ src/main/java/fr/ducruetl/BOTCPlugin/listeners/PlayerListener.java
@@ -0,0 +1,40 @@
+package fr.ducruetl.BOTCPlugin.listeners;
+
+import org.bukkit.ChatColor;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.player.PlayerJoinEvent;
+import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.scoreboard.Team;
+
+import fr.ducruetl.BOTCPlugin.BOTCPlugin;
+
+public class PlayerListener implements Listener {
+
+    private final BOTCPlugin plugin;
+
+    public PlayerListener(BOTCPlugin plugin) {
+        this.plugin = plugin;
+    }
+    
+    @EventHandler
+    public void onPlayerJoin(PlayerJoinEvent event) {
+        String playerName = event.getPlayer().getName();
+        event.setJoinMessage(ChatColor.GREEN + "" + ChatColor.BOLD + playerName 
+                            + ChatColor.RESET + ChatColor.GREEN + " entre dans le village.");
+
+        // Add player to the nametag team on join to be able to hide their nametag
+        Team nametagTeam = plugin.getNametagTeam();
+        if (!nametagTeam.hasEntry(playerName)) {
+            nametagTeam.addEntry(playerName);
+        }
+    }
+
+    @EventHandler
+    public void onPlayerQuit(PlayerQuitEvent event) {
+        String playerName = event.getPlayer().getName();
+        event.setQuitMessage(ChatColor.RED + "" + ChatColor.BOLD + playerName 
+                            + ChatColor.RESET + ChatColor.RED + " quitte le village. :(");
+    }
+    
+}
